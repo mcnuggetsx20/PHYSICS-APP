3d31994f663ec61bf3d91ce701bcc9e754833124
pazdanmichal <91290214+pazdanmichal@users.noreply.github.com>
2021-09-25 17:58:19 +0200

    Add files via upload

--- physics_app/functions.py
+++ physics_app/functions.py
@@ -6,6 +6,7 @@ def circle_line(start_pos,end_pos, lenght_of_first_line):
     y1 = start_pos[1]
     x2 = end_pos[0]
     y2 = end_pos[1]
+    r = lenght_of_first_line
     if x2-x1 == 0:
         x2_solved1 = x1
         y2_solved1 = y1-lenght_of_first_line
@@ -14,19 +15,26 @@ def circle_line(start_pos,end_pos, lenght_of_first_line):
     else:
         a_parameter = (y2-y1)/(x2-x1)
         b_parameter = y1-a_parameter*x1
-        r = lenght_of_first_line
         delta = (2*a_parameter*b_parameter-2*x1-2*y1*a_parameter)**2-4*(1+a_parameter**2)*(x1**2+b_parameter**2+y1**2-2*y1*b_parameter-r**2)
         delta_sqrt = math.sqrt(delta)
         x2_solved1 = (-(2*a_parameter*b_parameter-2*x1-2*y1*a_parameter)+delta_sqrt)/(2*(1+a_parameter**2))
         x2_solved2 = (-(2*a_parameter*b_parameter-2*x1-2*y1*a_parameter)-delta_sqrt)/(2*(1+a_parameter**2))
         y2_solved1 = a_parameter*x2_solved1 + b_parameter
         y2_solved2 = a_parameter*x2_solved2 + b_parameter
-    if (x2-x2_solved1)**2 + (y2-y2_solved1)**2 > (x2-x2_solved2)**2 + (y2-y2_solved2)**2:
-        x2_solved = x2_solved2
-        y2_solved = y2_solved2
+    if r > 0:
+        if (x2-x2_solved1)**2 + (y2-y2_solved1)**2 > (x2-x2_solved2)**2 + (y2-y2_solved2)**2:
+            x2_solved = x2_solved2
+            y2_solved = y2_solved2
+        else:
+            x2_solved = x2_solved1
+            y2_solved = y2_solved1
     else:
-        x2_solved = x2_solved1
-        y2_solved = y2_solved1
+        if (x2-x2_solved1)**2 + (y2-y2_solved1)**2 < (x2-x2_solved2)**2 + (y2-y2_solved2)**2:
+            x2_solved = x2_solved2
+            y2_solved = y2_solved2
+        else:
+            x2_solved = x2_solved1
+            y2_solved = y2_solved1
     return (x2_solved,y2_solved)
 
 def draw_coil (screen,start_pos,end_pos, line_width, lenght_of_first_line, number_of_links,link_radius,link_width,coil_color):
@@ -71,6 +79,62 @@ def a_t(A,k,m,t,fi):
     omega = (k/m)**0.5
     a = -A*omega*omega*math.sin(omega*t + fi)
     return a
+
+def moving_blocks(list_of_coils,list_of_weights,dict_of_connections,speed_multipler,fps,gravitation):
+    list_of_keys = (list(dict_of_connections.keys()))
+    new_list_of_coils = list_of_coils
+    new_list_of_weights = list_of_weights
+    for i in range (0, len(list_of_keys)):
+        weight_idx = list_of_keys[i]
+        weight_pos_x = list_of_weights[weight_idx][0][0]
+        weight_pos_y = list_of_weights[weight_idx][0][1]
+        weight_mass = list_of_weights[weight_idx][1]
+        weight_velocity_vector = list_of_weights[weight_idx][2]
+
+        list_of_connected_coils = dict_of_connections.get(weight_idx)
+        force_vectors = []
+        for coil in list_of_connected_coils:
+            coil_idx = coil[0]
+            coil_start_pos_x = list_of_coils[coil_idx][0][0]
+            coil_start_pos_y = list_of_coils[coil_idx][0][1]
+            coil_end_pos_x = list_of_coils[coil_idx][1][0]
+            coil_end_pos_y = list_of_coils[coil_idx][1][1]
+            coil_base_lenght = list_of_coils[coil_idx][2]
+            coil_k = list_of_coils[coil_idx][3]
+            coil_lenght = ((coil_end_pos_x-coil_start_pos_x)**2+ (coil_end_pos_y-coil_start_pos_y)**2)**0.5
+            f = coil_k * (coil_lenght-coil_base_lenght)
+            #print(coil_lenght-coil_base_lenght)
+            
+            if coil[1] == 0:
+                vector_start_pos = (coil_start_pos_x,coil_start_pos_y)
+                vector_end_pos = circle_line((coil_start_pos_x,coil_start_pos_y),(coil_end_pos_x,coil_end_pos_y),f)
+            elif coil[1] == 1:
+                vector_start_pos = (coil_end_pos_x,coil_end_pos_y)
+                vector_end_pos = circle_line((coil_end_pos_x,coil_end_pos_y),(coil_start_pos_x,coil_start_pos_y),f)
+                
+            force_vectors.append([vector_start_pos,vector_end_pos])
+        force_vectors.append([(weight_pos_x,weight_pos_y),(weight_pos_x,weight_pos_y+weight_mass*gravitation)])
+        result_vector = [0,0]
+        for j in force_vectors:
+            result_vector[0] += j[1][0]-j[0][0]
+            result_vector[1] += j[1][1]-j[0][1]
+        vector_value = (result_vector[0]**2+result_vector[1]**2)**0.5
+        delta_v = (vector_value/weight_mass)*(1/fps)
+        #print(result_vector)
+        vector_v = circle_line((weight_pos_x,weight_pos_y),(weight_pos_x+int(result_vector[0]),weight_pos_y+int(result_vector[1])),delta_v*speed_multipler)
+
+        weight_velocity_vector[0]+=vector_v[0]-weight_pos_x
+        weight_velocity_vector[1]+=vector_v[1]-weight_pos_y
+        x_result_pos = weight_velocity_vector[0]/fps
+        y_result_pos = weight_velocity_vector[1]/fps
+        result_pos = (weight_pos_x+x_result_pos,weight_pos_y+y_result_pos)
+        new_list_of_weights[weight_idx][0] = result_pos
+        for coil in list_of_connected_coils:
+            if coil[0] == 0:
+                new_list_of_coils[coil[0]][1] = result_pos
+            else:
+                new_list_of_coils[coil[0]][0] = result_pos
+    return(new_list_of_coils,new_list_of_weights)
 
 
 
